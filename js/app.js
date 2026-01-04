a6dd817767980b2b68b16be07da1e47475d09661
Abdumajidov2005 <otkirbekabdumajidov28@gmail.com>
2026-01-04 22:26:58 +0500

    update

--- js/app.js
+++ js/app.js
@@ -53,3 +53,24 @@
     ],
   });
 })();
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+
+  slidesPerView: "auto",
+  spaceBetween: 20,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  autoplay: {
+    delay: 3000,
+  },
+});
